af288a699c794cb66cec1eebe2f108b57e3cffa5
zubairmh <zubair.mh@protonmail.com>
2023-06-19 19:48:47 +0530

    Add Memory and disk usage

--- go.mod
+++ go.mod
@@ -1,10 +1,21 @@
-module github.com/zubairmh/compress
+module github.com/zubairmh/zfetch
 
 go 1.20
 
 require (
+	github.com/shirou/gopsutil/v3 v3.23.5
+	github.com/urfave/cli/v2 v2.25.6
+)
+
+require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.25.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"math"
 	"os"
+	"os/user"
 
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/urfave/cli/v2"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func n(size uint64) float64 {
+	return roundFloat(float64(size)*math.Pow(1024, -3), 2)
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
+
 func main() {
-	(&cli.App{}).Run(os.Args)
+	app := &cli.App{
+		Name:  "ZFetch",
+		Usage: "Display current system stats",
+		Action: func(*cli.Context) error {
+			u, _ := user.Current()
+			fmt.Println(u.Username)
+			v, _ := mem.VirtualMemory()
+			parts, _ := disk.Partitions(true)
+			var free, total uint64
+			for _, part := range parts {
+				device := part.Mountpoint
+				usage, _ := disk.Usage(device)
+				free += usage.Free
+				total += usage.Total
+
+			}
+
+			// almost every return value is a struct
+			fmt.Printf("Memory Total: %vGB, Free:%vGB, UsedPercent:%d%%\n", n(v.Total), n(v.Free), int(v.UsedPercent))
+			fmt.Printf("Disk Total: %vGB, Free:%vGB", n(total), n(free))
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
